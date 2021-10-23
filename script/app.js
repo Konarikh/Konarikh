0bfd44e84c53851f227fb369f4476b2d6a340ba5
Pavel Konarikh <paveldr9514@gmail.com>
2021-10-23 21:43:10 +0700

    first commit

---
+++ script/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm(
+//     "Are your here"
+// );
+
+// const answer = prompt("Ваместь 18 лет", "");
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Какая у вас фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
